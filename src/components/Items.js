d5670f7938e09a2ebbbc6eb1b6ba1b1e20834b60
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-23 00:27:17 +0530

    Adds static pagination

--- src/components/Items.js
+++ src/components/Items.js
@@ -12,18 +12,35 @@ const Items = () => {
     getData();
   }, []);
 
+  const pageHandler = () => {};
+
   return (
     <div>
       {products.length > 0 && (
-        <div className="products">
-          {products.map((product) => {
-            return (
-              <div className="products__single" key={product.id}>
-                <img src={product.thumbnail} alt={product.tile} />
-                <span>{product.title}</span>
-              </div>
-            );
-          })}
+        <div>
+          <div className="products">
+            {products.map((product) => {
+              return (
+                <div className="products__single" key={product.id}>
+                  <img src={product.thumbnail} alt={product.tile} />
+                  <span>{product.title}</span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="page__number__all">
+            <span className="page__number">◀️</span>
+            {[...Array(products.length / 10)].map((product, i) => (
+              <span
+                className="page__number"
+                key={i}
+                onClick={() => pageHandler()}
+              >
+                {i + 1}
+              </span>
+            ))}
+            <span className="page__number">▶️</span>
+          </div>
         </div>
       )}
     </div>
