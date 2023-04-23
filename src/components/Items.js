27fee049f81ff9a13ca2818e94488318cdfd3859
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-23 15:48:42 +0530

    Renders only 10 items at a time -Makes pagination functional with boundary checks as well

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../App.css";
 const Items = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const getData = async () => {
     const data = await fetch("https://dummyjson.com/products?limit=100");
@@ -12,14 +13,17 @@ const Items = () => {
     getData();
   }, []);
 
-  const pageHandler = () => {};
+  const pageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= 10 && selectedPage !== page)
+      setPage(selectedPage);
+  };
 
   return (
     <div>
       {products.length > 0 && (
         <div>
           <div className="products">
-            {products.map((product) => {
+            {products.slice(page * 10 - 10, page * 10).map((product) => {
               return (
                 <div className="products__single" key={product.id}>
                   <img src={product.thumbnail} alt={product.tile} />
@@ -29,17 +33,31 @@ const Items = () => {
             })}
           </div>
           <div className="page__number__all">
-            <span className="page__number">◀️</span>
+            <span
+              className="page__number"
+              onClick={() => pageHandler(page - 1)}
+            >
+              ◀️
+            </span>
             {[...Array(products.length / 10)].map((product, i) => (
               <span
-                className="page__number"
+                className={
+                  page === i + 1
+                    ? "page__number page__selected"
+                    : "page__number"
+                }
                 key={i}
-                onClick={() => pageHandler()}
+                onClick={() => pageHandler(i + 1)}
               >
                 {i + 1}
               </span>
             ))}
-            <span className="page__number">▶️</span>
+            <span
+              className="page__number"
+              onClick={() => pageHandler(page + 1)}
+            >
+              ▶️
+            </span>
           </div>
         </div>
       )}
